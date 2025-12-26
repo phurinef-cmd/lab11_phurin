2f5d06d7d6de9963ea49ecdf744014a122d59acb
Ef <phurin.ef@gmail.com>
2025-12-26 17:12:05 +0700

    lab11_1 complete

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,29 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+// cout << "Press Enter 3 times to reveal your future.";
+// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<ctime>
+using namespace std;
+
+int main(){
+    string space;
+    srand(time(0));
+    int i = 0 ;
+    cout << "Press Enter 3 times to reveal your future.";
+    while(i<3){
+        cin >> space;
+        i++;
+    }
+    int x = rand()%9;
+    if(x == 0) cout << "You will get A in this 261102.";
+    else if(x == 1) cout << "You will get B+ in this 261102.";
+    else if(x == 2) cout << "You will get B in this 261102.";
+    else if(x == 3) cout << "You will get C+ in this 261102.";
+    else if(x == 4) cout << "You will get C in this 261102.";
+    else if(x == 5) cout << "You will get D+ in this 261102.";
+    else if(x == 6) cout << "You will get D in this 261102.";
+    else if(x == 7) cout << "You will get F in this 261102.";
+    else if(x == 8) cout << "You will get W in this 261102.";
+}
